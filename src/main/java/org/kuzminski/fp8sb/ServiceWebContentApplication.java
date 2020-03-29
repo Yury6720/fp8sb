432ff0d6888e3de25f6f1a3cf8d4c4cac82c9881
Yurij <kuzminskij5683415@gmail.com>
2020-03-30 02:12:23 +0300

    Added -Application starter class -first controller for "Home Page" -"Home Page"

---
+++ src/main/java/org/kuzminski/fp8sb/ServiceWebContentApplication.java
@@ -0,0 +1,11 @@
+package org.kuzminski.fp8sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceWebContentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceWebContentApplication.class, args);
+    }
+}
